70c81735fe0d77c37f646af06cea07e731550bba
winches <329487092@qq.com>
2022-11-22 22:48:42 +0800

    feat: add exchangeWords command

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-import { window } from 'vscode'
+import { commands, window } from 'vscode'
 
 export function activate() {
-  window.showInformationMessage('Hello')
+  commands.registerCommand('exchangeWords.test', () => {
+    window.showInformationMessage('test')
+  })
+  commands.registerTextEditorCommand('exchangeWords', (textEdit, edit) => {
+    const document = textEdit.document
+    const firstSelections = textEdit.selections[0]
+    const secondSelections = textEdit.selections[1]
+    const firstText = document.getText(firstSelections)
+    const secondText = document.getText(secondSelections)
+
+    edit.replace(firstSelections, secondText)
+    edit.replace(secondSelections, firstText)
+  })
 }
 
 export function deactivate() {
